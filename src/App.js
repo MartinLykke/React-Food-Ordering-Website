d477dca0aeab20540b5bbaef587f26bb6e7a35fa
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-01-27 18:27:12 +0100

    Navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
