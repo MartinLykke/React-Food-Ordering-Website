0652f9f91eaafdc5b172c6e2d2eaa87e2c11f043
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-02-18 10:36:45 +0100

    Landing page updated, looks good on pc and mobile

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./LandingPageElements";
+function LandingPage() {
+  return (
+    <HeroContainer>
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Bæredygtig mad</HeroH1>
+          <HeroP>Leveret lige til døren</HeroP>
+          <HeroBtn>Se menuen</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default LandingPage;
