e865c2f6edd365c1f3fde06a5cc4fc073a7fc71e
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-03-04 11:58:27 +0100

    two dishes are displayed at a time

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,30 +6,51 @@ import useFirestore from "../hooks/useFireStore";
 const Cards = () => {
   const { docs } = useFirestore("images");
 
-  return (
-    <div className="cards">
-      {localStorage.getItem("loginEmail") === "admin@gmail.com" && (
-        <UploadForm />
-      )}
+  //splits into 2
+  var chunks = function (array, size = 2) {
+    var results = [];
+    while (array.length) {
+      results.push(array.splice(0, size));
+    }
+    return results;
+  };
+  const data = chunks(docs, 2);
 
-      <h1>Dagens retter</h1>
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          {docs &&
-            docs.map((doc) => (
-              <CardItem
-                src={doc.url}
-                key={doc.id}
-                text={doc.imageText}
-                amountLeft={doc.amountLeft}
-                label={doc.imageLabel}
-                price={doc.price}
-                id={doc.id}
-              ></CardItem>
-            ))}
+  return (
+    <>
+      <div className="cards">
+        {localStorage.getItem("loginEmail") === "admin@gmail.com" && (
+          <UploadForm />
+        )}
+
+        <h1>Dagens retter</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <div>
+              {data.map((childs, index) => {
+                return (
+                  <ul className="cards__items">
+                    {childs.map((c, cindex) => {
+                      return (
+                        <CardItem
+                          src={c.url}
+                          key={c.id}
+                          text={c.imageText}
+                          amountLeft={c.amountLeft}
+                          label={c.imageLabel}
+                          price={c.price}
+                          id={c.id}
+                        ></CardItem>
+                      );
+                    })}
+                  </ul>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
