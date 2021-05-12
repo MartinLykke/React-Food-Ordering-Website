3cf9a83149430f7c8683e5185eec691928a24af4
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-05-12 20:31:40 +0200

    Cart page looks much better

--- src/Components/CartFill.js
+++ src/Components/CartFill.js
@@ -1,46 +1,85 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { CartContext } from "./CartContext";
+import { MinusCircle, PlusCircle } from "react-feather";
+function CartFill(props) {
+  const [cart, setCart] = useContext(CartContext);
+  const currency = "kr.";
+  const zeroes = ",00 ";
+  const fullPrice = zeroes + currency;
+  const deliveryCost = 29;
+  let totalPrice =
+    cart.reduce(
+      (acc, curr) => acc + parseInt(curr.price * curr.qty),
+      deliveryCost
+    ) + deliveryCost;
+  useEffect(() => {
+    totalPrice =
+      cart.reduce((acc, curr) => acc + parseInt(curr.price), 0) + deliveryCost;
+  }, [cart]);
+  const removeFromOrder = (id) => {
+    const newCart = cart.filter((Cart) => Cart.id !== id);
+    setCart(newCart);
+  };
+  const increaseBasket = (id) => {
+    setCart(
+      cart.map((CartItem) =>
+        CartItem.id === id ? { ...CartItem, qty: CartItem.qty + 1 } : CartItem
+      )
+    );
+  };
 
-function CartFill() {
   return (
     <div className="container mt-5">
       <div className="row">
         <div class="col-md-4 order-md-2 mb-4">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
             <span class="text-muted">Din kurv</span>
-            <span class="badge badge-secondary badge-pill">3</span>
+            <span class="badge badge-secondary badge-pill">{cart.length}</span>
           </h4>
           <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
+            {cart.map((doc) => (
+              <li class="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                  <h6 class="my-0">
+                    {" "}
+                    {doc.name}{" "}
+                    <PlusCircle
+                      onClick={() => increaseBasket(doc.id)}
+                      className="cursor-pointer"
+                      size={18}
+                      color={"var(--clr-green-dark)"}
+                    />
+                    <a>{doc.qty}</a>
+                    <MinusCircle
+                      onClick={() => props.decreaseBasket(doc.id, doc.qty)}
+                      className="cursor-pointer"
+                      size={18}
+                      color={"var(--clr-red-dark)"}
+                    />
+                  </h6>
+
+                  <small class="text-muted"></small>
+                </div>
+                <span class="text-muted">
+                  {doc.price}
+                  {fullPrice}
+                </span>
+              </li>
+            ))}
+
             <li class="list-group-item d-flex justify-content-between bg-light">
               <div class="text-success">
                 <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
+                <small>saveameal</small>
               </div>
-              <span class="text-success">-$5</span>
+              <span class="text-success">-14{fullPrice}</span>
             </li>
             <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
+              <span>Total (DKK)</span>
+              <strong>
+                {totalPrice}
+                {fullPrice}
+              </strong>
             </li>
           </ul>
 
@@ -243,7 +282,8 @@ function CartFill() {
             </div>
             <hr class="mb-4" />
             <button class="btn btn-primary btn-lg btn-block" type="submit">
-              Bestil og betal med kort (00,00 kr.)
+              Bestil og betal med kort ({totalPrice}
+              {fullPrice})
             </button>
           </form>
         </div>
