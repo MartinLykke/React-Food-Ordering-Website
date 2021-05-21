87589bc07add148b42c574f139b9c79260bbb0c3
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-05-21 10:27:55 +0200

    cookies only need to be accepted once

--- src/Components/AllowCookies.js
+++ src/Components/AllowCookies.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function AllowCookies() {
-  const [showComp, setShowComp] = useState(true);
+  const [showComp, setShowComp] = useState(() =>
+    getLocalStorage("showCookiesPrompt", true)
+  );
+
   const onClick = () => setShowComp(false);
+  function setLocalStorage(key, value) {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      // catch possible errors:
+      // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+    }
+  }
+
+  function getLocalStorage(key, initialValue) {
+    try {
+      const value = window.localStorage.getItem(key);
+      return value ? JSON.parse(value) : initialValue;
+    } catch (e) {
+      // if error, return initial value
+      return initialValue;
+    }
+  }
+  useEffect(() => {
+    setLocalStorage("showCookiesPrompt", showComp);
+  }, [showComp]);
   return (
     <div>
       {showComp && (
         <div className="fixed-bottom">
-          <div className="d-flex justify-content-center container mt-5">
+          <div className="d-flex justify-content-center ">
             <div className="">
               <div className="col-md-12">
                 <div className="d-flex flex-row justify-content-between align-items-center card cookie p-3">
