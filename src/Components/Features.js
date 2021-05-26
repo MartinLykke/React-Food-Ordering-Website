eb2df7fd7f664b841a4e1c9ede8bd4e702f7644a
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-05-26 16:02:43 +0200

    features updated

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -1,66 +1,35 @@
 import React from "react";
-
+import { Map, MapPin, User, UserPlus } from "react-feather";
+import { Link } from "react-router-dom";
+import "./Features.css";
 function Features() {
   return (
-    <div class="container px-4 py-5" id="featured-3">
-      <h2 class="pb-2 border-bottom">Columns with icons</h2>
+    <div class="container px-4 pb-4" id="featured-3">
+      <h3 class="pb-2 border-bottom text-center">Det er så nemt.</h3>
       <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
         <div class="feature col">
           <div class="feature-icon bg-primary bg-gradient">
-            <svg class="bi" width="1em" height="1em">
-              <use />
-            </svg>
+            <i class="fas fa-hamburger"></i>
           </div>
-          <h2>Featured title</h2>
+          <h2>Læg retter i kurven</h2>
           <p>
-            Paragraph of text beneath the heading to explain the heading. We'll
-            add onto it with another sentence and probably just keep going until
-            we run out of words.
+            Hvad kunne du tænke dig? Se dagens retter her, eller gå til{" "}
+            <Link to="/menu">menuen</Link>.
           </p>
-          <a href="#" class="icon-link">
-            Call to action
-            <svg class="bi" width="1em" height="1em">
-              <use />
-            </svg>
-          </a>
         </div>
         <div class="feature col">
           <div class="feature-icon bg-primary bg-gradient">
-            <svg class="bi" width="1em" height="1em">
-              <use />
-            </svg>
+            <UserPlus size={35} color="white"></UserPlus>
           </div>
-          <h2>Featured title</h2>
-          <p>
-            Paragraph of text beneath the heading to explain the heading. We'll
-            add onto it with another sentence and probably just keep going until
-            we run out of words.
-          </p>
-          <a href="#" class="icon-link">
-            Call to action
-            <svg class="bi" width="1em" height="1em">
-              <use />
-            </svg>
-          </a>
+          <h2>Log ind</h2>
+          <p>Log hurtigt ind med google og indtast dit vejnavn.</p>
         </div>
         <div class="feature col">
           <div class="feature-icon bg-primary bg-gradient">
-            <svg class="bi" width="1em" height="1em">
-              <use />
-            </svg>
+            <i class="fas fa-motorcycle"></i>
           </div>
-          <h2>Featured title</h2>
-          <p>
-            Paragraph of text beneath the heading to explain the heading. We'll
-            add onto it with another sentence and probably just keep going until
-            we run out of words.
-          </p>
-          <a href="#" class="icon-link">
-            Call to action
-            <svg class="bi" width="1em" height="1em">
-              <use />
-            </svg>
-          </a>
+          <h2>Betal og få din mad leveret</h2>
+          <p>Betal kontant eller med kreditkort. Velbekomme!</p>
         </div>
       </div>
     </div>
